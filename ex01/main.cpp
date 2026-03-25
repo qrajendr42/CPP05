3a4884af0bb55e5bdabba1a9510a92185a2523e5
qrajendr <qrajendr@DESKTOP-0QO0FLR.localdomain>
2026-03-25 17:51:13 +0800

    hoho

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,26 @@
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main()
 {
+    try
+    {
+        Bureaucrat b("111", 50);
+        Form f("222", 40, 30);
+
+        std::cout << b << std::endl;
+        std::cout << f << std::endl;
+
+        b.signForm(f);
+
+        Bureaucrat b2("333", 30);
+        b2.signForm(f);
+        
+        std::cout << f << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return (0);
 }
