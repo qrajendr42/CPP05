525ae2e55eb223f815f577edb08cb2362941f36c
Qhaby <qhabylen@gmail.com>
2026-04-07 15:44:58 +0800

    Rename Bureaucrat instances for clarity

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     try
     {
-        Bureaucrat b("111", 50); //declaring b
+        Bureaucrat b("Ali", 50); //declaring b
         Form f("222", 40, 30); //sign check, cannot sign cuz no bureu declared
 
         std::cout << b << std::endl;
@@ -13,7 +13,7 @@ int main()
 
         b.signForm(f); //b sign check, should reject, cause too low
 
-        Bureaucrat c("333", 30); //declaring c
+        Bureaucrat c("Abu", 30); //declaring c
         c.signForm(f); //c sign check, should accept, cause fulfilled requirement
 
         std::cout << f << std::endl;
